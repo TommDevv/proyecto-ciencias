81e44b5b33fc2f97d725429654ad05cbd40a9595
TommDevv <162940383+TommDevv@users.noreply.github.com>
2025-09-08 08:16:32 -0500

    hash fix

--- src/app/busqueda-interna/hash/hash.component.ts
+++ src/app/busqueda-interna/hash/hash.component.ts
@@ -17,7 +17,7 @@ export class HashComponent {
   anadirFinalizado: boolean;
   buscarFinalizado: boolean;
   eliminarFinalizado: boolean;
-  tamanoClave: number;
+  funcionHash: string;
 
   constructor(){
     this.cantreg = 0;
@@ -31,7 +31,7 @@ export class HashComponent {
     this.buscarFinalizado = false;
     this.eliminarFinalizado = false;
     this.resultado = '';
-    this.tamanoClave = 1;
+    this.funcionHash = '';
   }
 
   generarEstructura(): void{
@@ -46,64 +46,116 @@ export class HashComponent {
 
   anadir(): void{
     this.limpiar();
-    if(this.ingreso.length != this.tamanoClave){
+    if(this.ingreso.length != 4){
       alert('Tamaño de la clave incorrecto')
       return
     }
-    var i = 0;
-    var flag: boolean = true;
-      while(flag){
-        if(this.values[i] === ''){
-          this.resultado = `Valor ${this.values[i]} ingresado en la posición ${i}`
-          this.values[i] = this.ingreso;
-          flag = false;
-        }else{
-          i++
-        }
-      }
-    this.contador = i;
+    var index = 0;
+
+    switch(this.funcionHash){
+      case 'mod':
+        index = this.hashMod(Number(this.ingreso), this.values.length);
+        break;
+      case 'cuadrado':
+        index = this.hashCuad(Number(this.ingreso));
+        break;
+      case 'truncamiento':
+        index = this.hashTrunc(Number(this.ingreso));
+        break;
+      case 'plegamiento':
+        index = this.hashPleg(Number(this.ingreso));
+        break;
+      default:
+        alert('Seleccione una función hash')
+        return
+    }
+
+    if (index >= this.values.length){
+      index = index - this.values.length;
+    }
+
+    this.values[index] = this.ingreso;
+    this.resultado = `Valor ${this.values[index]} ingresado en la posición ${index}`;
     this.anadirFinalizado = true;
   }
 
   buscar(): void{
     this.limpiar();
-    var i = 0;
-    var flag: boolean = true;
-      while(flag){
-        if(this.values[i] === this.busqueda){
-          this.resultado = `Valor ${this.values[i]} encontrado en la posición ${i}`
-          flag = false;
-        }else if(i === this.values.length - 1){
-          flag = false;
-          this.resultado = ' Valor no encontrado en la lista'
-        }
-        else {
-          i++
-        }
-      }
-    this.contador = i;
-    this.buscarFinalizado = true;
+
+    var index = 0;
+
+    switch(this.funcionHash){
+      case 'mod':
+        index = this.hashMod(Number(this.busqueda), this.values.length);
+        break;
+      case 'cuadrado':
+        index = this.hashCuad(Number(this.busqueda));
+        break;
+      case 'truncamiento':
+        index = this.hashTrunc(Number(this.busqueda));
+        break;
+      case 'plegamiento':
+        index = this.hashPleg(Number(this.busqueda));
+        break;
+      default:
+        alert('Seleccione una función hash')
+        return
+    }
+
+    if (index >= this.values.length){
+      index = index - this.values.length;
+    }
+
+
+    if(this.values[index] === ''){
+      this.resultado = ' Valor no encontrado en la lista'
+      this.buscarFinalizado = true;
+      return;
+    } else {
+      this.resultado = `Valor ${this.values[index]} encontrado en la posición ${index}`;
+      this.buscarFinalizado = true;
+      return;
+    }
+
   }
 
   eliminar(): void{
     this.limpiar();
-    var i = 0;
-    var flag: boolean = true;
-      while(flag){
-        if(this.values[i] === this.retiro){
-          this.values[i] = '';
-          this.resultado = `Valor ${this.values[i]} eliminado en la posición ${i}`
-          flag = false;
-        }else if(i === this.values.length -1){
-          flag = false;
-          this.resultado = ' Valor no encontrado en la lista'
-        }
-        else{
-          i++
-        }
-      }
-    this.contador = i;
-    this.eliminarFinalizado = true;
+    
+    var index = 0;
+
+    switch(this.funcionHash){
+      case 'mod':
+        index = this.hashMod(Number(this.retiro), this.values.length);
+        break;
+      case 'cuadrado':
+        index = this.hashCuad(Number(this.retiro));
+        break;
+      case 'truncamiento':
+        index = this.hashTrunc(Number(this.retiro));
+        break;
+      case 'plegamiento':
+        index = this.hashPleg(Number(this.retiro));
+        break;
+      default:
+        alert('Seleccione una función hash')
+        return
+    }
+
+    if (index >= this.values.length){
+      index = index - this.values.length;
+    }
+
+    if(this.values[index] === ''){
+      this.resultado = ' Valor no encontrado en la lista'
+      this.eliminarFinalizado = true;
+      return;
+    } else {
+      this.values[index] = '';
+      this.resultado = `Valor ${this.retiro} eliminado en la posición ${index}`;
+      this.eliminarFinalizado = true;
+      return;
+    }
   }
 
   limpiar(){
